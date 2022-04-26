5f269d5fa9fd197af712dadd26251424c4539e44
Favour Favalli <Favallifav@gmail.com>
2022-04-26 23:38:18 +0100

    Add loop and scorecount.

--- rps.js
+++ rps.js
@@ -6,17 +6,31 @@ let computerPlay = randomItem;
     
 console.log(computerPlay);
 
+let playerScore = 0; 
+let computerScore = 0;
+
+for (let i = 0; i < 5; i++){
 let playerSelection = prompt("rock, paper, scissors");
+
+
 
 let playerInput = playerSelection
 
 console.log(playerInput);
 
-function playRound(playerInput,computerPlay) {if((computerPlay == "rock") && (playerInput == 'paper')) {alert("you win, paper beats rock")} else if(computerPlay == "rock" && playerInput == 'rock') {alert("Tie")}else if(computerPlay == "rock" && playerInput == 'scissors') {alert("You lose, Rock beats Scissors")} else if(computerPlay == "paper" && playerInput == 'rock') {alert("You lose, Paper beats Rock")} else if(computerPlay == "paper" && playerInput == 'paper') {alert("Tie")} else if(computerPlay == "paper" && playerInput == 'scissors') {alert("You win, Scissors beats Paper")} else if(computerPlay == "scissors" && playerInput == 'rock') {alert("You win, Rock beats Scissors")} else if(computerPlay == "scissors" && playerInput == 'paper') {alert("You lose, Scissors beats Paper")} else if(computerPlay == "scissors" && playerInput == 'scissors') {alert("Tie")} else{alert("Error!  Try again.")}
-}
 
+function playRound(playerInput,computerPlay) {if((computerPlay == "rock") && (playerInput == 'paper')) {alert("you win, paper beats rock"); ++playerScore} else if(computerPlay == "rock" && playerInput == 'rock') {alert("Tie")}else if(computerPlay == "rock" && playerInput == 'scissors') {alert("You lose, Rock beats Scissors"); ++computerScore} else if(computerPlay == "paper" && playerInput == 'rock') {alert("You lose, Paper beats Rock");++computerScore} else if(computerPlay == "paper" && playerInput == 'paper') {alert("Tie")} else if(computerPlay == "paper" && playerInput == 'scissors') {alert("You win, Scissors beats Paper"); ++playerScore} else if(computerPlay == "scissors" && playerInput == 'rock') {alert("You win, Rock beats Scissors"); ++playerScore} else if(computerPlay == "scissors" && playerInput == 'paper') {alert("You lose, Scissors beats Paper"); ++computerScore} else if(computerPlay == "scissors" && playerInput == 'scissors') {alert("Tie")} else{alert("Error!  Try again.")}
+}
 
 
 
 const computerSelection = computerPlay; 
 console.log(playRound(computerPlay,playerInput));
+
+}
+
+
+alert("Playerscore"); 
+alert(playerScore);
+alert("Computerscore");
+alert(computerScore);
